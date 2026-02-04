f5d1dd59d0e03bad83c07fca7d369a85b7274c18
Saurav <ghimiresaurav789@gmail.com>
2026-02-04 14:21:46 +0545

    chore: made a reset button function

--- script.js
+++ script.js
@@ -1,35 +1,88 @@
-let player1 = 'X';
-let player2 = 'O';
+let currentPlayer = 'X';
 
 let running = true;
-const game = () => {
-    if( running === true) {
-        console.log('it is ruunig');
-    } else {
-        console.log('it is over');
-    }
-}
 
 const board = [
     '', '', '', '', '', '', '', '', ''
-]
+];
 
 
 const winningConditions = [
     [0,1,2], [3,4,5], [6,7,8],
     [0,3,6], [1,4,7], [2,5,8],
     [0,4,8], [2,4,6]
-]
+];
 
 const cells = document.querySelectorAll('.child-box');
 
 const statusDisplay = document.querySelector('#status');
 
-const cellclicked = (e) => {
-    console.log('A box is clicked', e);
+const cellclicked = (e)  => {
 
+    const index = e.target.id.slice(-1) - 1;
+
+    if (running === true && e.target.innerText === '') {
+        board[index] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+
+        checkWinner();
+
+        if (running && board.every(cell => cell !== '')) {
+            running = false;
+            statusDisplay.innerText = "It's a Draw!";
+            return; 
+        }
+
+        if ( running ) {
+
+        if (currentPlayer === 'X') {
+            currentPlayer ='O'
+        } else {
+            currentPlayer ='X'
+        }
+
+        statusDisplay.innerText = 'Its ' + currentPlayer + ' turn';
+    }
+    }
 }
     
 cells.forEach( box => {
     box.addEventListener('click', cellclicked);
 });
+
+const checkWinner = () => {
+    for (let i = 0; i < winningConditions.length; i ++) {
+        let a = board[winningConditions[i][0]]
+        let b = board[winningConditions[i][1]]
+        let c = board[winningConditions[i][2]]
+
+        if (a === '' ){
+            continue;
+        } else if (a === b && b === c) {
+            
+            running = false;
+
+            statusDisplay.innerText = 'Player ' + currentPlayer + ' Wins'
+
+            return;
+        }
+    }
+}
+
+const resetBtn = document.querySelector('#reset-button');
+const restartGame = () => {
+    board.fill('');
+
+    cells.forEach(cell => cell.innerText = '');
+
+    currentPlayer ='X'
+
+    running = true;
+
+    statusDisplay.innerText = 'Its ' + currentPlayer + ' turn';
+}
+
+resetBtn.addEventListener('click', restartGame);
+
+
